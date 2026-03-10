51c8616529ed8b3fc7ed2ecef0812184266f1c11
czarny <czarny@atmoapps.com>
2026-03-10 13:43:31 +0100

    Initial commit

---
+++ test/aws-build.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsBuild from '../lib/aws-build-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-build-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsBuild.AwsBuildStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
